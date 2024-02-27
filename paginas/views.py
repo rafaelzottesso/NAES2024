32b91bc7a6886faa18c5ccf6dd7310fdc432179f
Professor <rafael.zottesso@ifpr.edu.br>
2024-02-26 21:01:33 -0300

    Criação e configuração do projeto e do app paginas

---
+++ paginas/views.py
@@ -0,0 +1,6 @@
+from django.views.generic import TemplateView
+
+# Criar uma view para a página inicial
+# com herança para a classe TemplateView
+class IndexView(TemplateView):
+    template_name = "paginas/index.html"
