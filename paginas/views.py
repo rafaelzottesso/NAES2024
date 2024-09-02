463b017e3f143da7e3bc94fdeb56a4bf3235eef5
Professor <rafael.zottesso@ifpr.edu.br>
2024-09-02 20:35:02 -0300

    Personalizando consultas no Django; Querysets

--- paginas/views.py
+++ paginas/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from cadastros.models import Pessoa, Cidade
 
 # Criar uma view para a página inicial
 # com herança para a classe TemplateView
@@ -8,6 +9,15 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['titulo'] = "Página Inicial"
+        context['qtde_pessoas'] = Pessoa.objects.all().count()
+        
+        if(self.request.user.is_authenticated):
+            context['m_pessoas'] = Pessoa.objects.filter(
+                cadastrado_por=self.request.user).count()
+            
+        context['cidades'] = Cidade.objects.all().order_by('nome')
+        context['pessoa'] = Pessoa.objects.get(pk=1)
+                    
         return context
     
     
